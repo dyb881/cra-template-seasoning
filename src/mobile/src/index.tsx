a5f6eb3a18cfc27a7eb3834028140aa1e90f5510
dyb881 <15118995517@163.com>
2020-12-02 10:22:29 +0800

    去除 service worker

--- src/mobile/src/index.tsx
+++ src/mobile/src/index.tsx
@@ -1,21 +1,15 @@
 import 'common/compatible';
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+render(
   <StrictMode>
     <App />
   </StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
