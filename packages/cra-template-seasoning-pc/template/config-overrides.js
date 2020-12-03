531b3502d7f83e86c2f2fb403fed47bd7be5d299
dyb881 <15118995517@163.com>
2020-12-03 09:32:57 +0800

    修改全局 less 变量

--- packages/cra-template-seasoning-pc/template/config-overrides.js
+++ packages/cra-template-seasoning-pc/template/config-overrides.js
@@ -2,18 +2,18 @@ const { override, addWebpackPlugin, fixBabelImports, addBabelPlugin, addLessLoad
 const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
 
 const modifyVars = {
-  '@ra-p': '#1890ff', // 全局主色
-  '@ra-s': '#52c41a', // 成功色
-  '@ra-w': '#faad14', // 警告色
-  '@ra-e': '#f5222d', // 错误色
-  '@ra-fs': '16px', // 主字号
-  '@ra-c': 'rgba(0, 0, 0, 0.85)', // 主文本色
-  '@ra-cs': 'rgba(0, 0, 0, .45)', // 次文本色
-  '@ra-dc': 'rgba(0, 0, 0, .25)', // 失效色
-  '@ra-dcb': 'rgba(0, 0, 0, .05)', // 失效背景色
-  '@ra-bc': '#eee', // 边框色
-  '@ra-bcd': '#ccc', // 边框色-深色
-  '@ra-b': '#fff', // 背景色
+  '@s-p': '#1890ff', // 全局主色
+  '@s-s': '#52c41a', // 成功色
+  '@s-w': '#faad14', // 警告色
+  '@s-e': '#f5222d', // 错误色
+  '@s-fs': '16px', // 主字号
+  '@s-c': 'rgba(0, 0, 0, 0.85)', // 主文本色
+  '@s-cs': 'rgba(0, 0, 0, .45)', // 次文本色
+  '@s-dc': 'rgba(0, 0, 0, .25)', // 失效色
+  '@s-dcb': 'rgba(0, 0, 0, .05)', // 失效背景色
+  '@s-bc': '#eee', // 边框色
+  '@s-bcd': '#ccc', // 边框色-深色
+  '@s-b': '#fff', // 背景色
 };
 
 module.exports = override(
