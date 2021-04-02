c8e924c6aa26d01c8b038604eb998b1b103eab27
dyb881 <15118995517@163.com>
2021-04-02 11:04:46 +0800

    .

--- src/pc-admin/template/src/pages/files/configs/index.tsx
+++ src/pc-admin/template/src/pages/files/configs/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
 import { message, Button } from 'antd';
 import { PlusOutlined, DeleteOutlined } from '@ant-design/icons';
 import { InputList } from 'seasoning';
