9757e5c032f749fdc04c03825544e0c4835a4975
dyb881 <15118995517@163.com>
2021-11-18 14:30:37 +0800

    格式化

--- src/pc-admin/template/src/components/pageTable/virtual_table.tsx
+++ src/pc-admin/template/src/components/pageTable/virtual_table.tsx
@@ -13,95 +13,94 @@ export type TVirtualTableProps = TableProps<any> & {
 /**
  * 虚拟滚动表格
  */
-export const VirtualTable = combine<TVirtualTableProps>(
-  ({ stores, tableWidth = 0, minColumnWidth = 100, columns = [], ...props }) => {
-    const { componentSize = 'large' } = stores.layout.setting;
-    const gridRef = useRef<VariableSizeGrid>(null);
-    const { x = 0, y = 0 } = props.scroll || {};
+export const VirtualTable = combine<TVirtualTableProps>((_props) => {
+  const { stores, tableWidth = 0, minColumnWidth = 100, columns = [], ...props } = _props;
+  const { componentSize = 'large' } = stores.layout.setting;
+  const gridRef = useRef<VariableSizeGrid>(null);
+  const { x = 0, y = 0 } = props.scroll || {};
 
-    useEffect(() => {
-      gridRef.current?.resetAfterIndices({
-        rowIndex: 0,
-        columnIndex: 0,
-        shouldForceUpdate: false,
-      });
-    }, [tableWidth]);
-
-    // 给剩余的列分配宽度，若分配的宽度小于最小列宽，将启用最小列宽，并过滤 fixed
-    const mergedColumns = useMemo(() => {
-      const { length } = columns!.filter(({ width }) => !width);
-      const tmpWidth = length === 0 ? 0 : Math.floor((tableWidth - +x) / length);
-      const width = tmpWidth < minColumnWidth ? minColumnWidth : tmpWidth;
-      let mergedColumns = columns.map(({ fixed, ...column }) => (column.width ? column : { ...column, width }));
-
-      // 实际宽度大于滚动宽度时，放大列宽
-      const columnsWidth = width * length + +x;
-      if (tableWidth > columnsWidth) {
-        const scale = tableWidth / columnsWidth;
-        mergedColumns = mergedColumns.map(({ width, ...column }) => {
-          return { ...column, width: Math.floor(+width! * scale) };
-        });
-      }
-
-      return mergedColumns;
-    }, [x, tableWidth]);
-
-    const [connectObject] = useState(() => {
-      const obj = {};
-      Object.defineProperty(obj, 'scrollLeft', {
-        get: () => null,
-        set: (scrollLeft) => {
-          if (gridRef.current) {
-            gridRef.current!.scrollTo({
-              scrollLeft,
-            } as any);
-          }
-        },
-      });
-      return obj;
+  useEffect(() => {
+    gridRef.current?.resetAfterIndices({
+      rowIndex: 0,
+      columnIndex: 0,
+      shouldForceUpdate: false,
     });
+  }, [tableWidth]);
 
-    const renderVirtualList = (rawData: any, { ref, onScroll }: any) => {
-      ref.current = connectObject;
-      return (
-        <VariableSizeGrid
-          ref={gridRef}
-          className="virtual-grid"
-          height={+y}
-          rowCount={rawData.length}
-          columnCount={mergedColumns.length}
-          rowHeight={() => rowHeights[componentSize]}
-          width={tableWidth}
-          onScroll={({ scrollLeft }) => onScroll({ scrollLeft })}
-          columnWidth={(index) => +mergedColumns[index].width!}
-        >
-          {({ columnIndex, rowIndex, style }) => {
-            const { dataIndex, render } = mergedColumns[columnIndex] as any;
-            const data = rawData[rowIndex];
-            const column = data['' + dataIndex];
+  // 给剩余的列分配宽度，若分配的宽度小于最小列宽，将启用最小列宽，并过滤 fixed
+  const mergedColumns = useMemo(() => {
+    const { length } = columns!.filter(({ width }) => !width);
+    const tmpWidth = length === 0 ? 0 : Math.floor((tableWidth - +x) / length);
+    const width = tmpWidth < minColumnWidth ? minColumnWidth : tmpWidth;
+    let mergedColumns = columns.map(({ fixed, ...column }) => (column.width ? column : { ...column, width }));
 
-            return (
-              <div style={{ padding: columnPadding[componentSize], ...style }} className={styles.virtualColumn}>
-                {render?.(column ?? data, data, rowIndex) || column}
-              </div>
-            );
-          }}
-        </VariableSizeGrid>
-      );
-    };
+    // 实际宽度大于滚动宽度时，放大列宽
+    const columnsWidth = width * length + +x;
+    if (tableWidth > columnsWidth) {
+      const scale = tableWidth / columnsWidth;
+      mergedColumns = mergedColumns.map(({ width, ...column }) => {
+        return { ...column, width: Math.floor(+width! * scale) };
+      });
+    }
 
+    return mergedColumns;
+  }, [x, tableWidth]);
+
+  const [connectObject] = useState(() => {
+    const obj = {};
+    Object.defineProperty(obj, 'scrollLeft', {
+      get: () => null,
+      set: (scrollLeft) => {
+        if (gridRef.current) {
+          gridRef.current!.scrollTo({
+            scrollLeft,
+          } as any);
+        }
+      },
+    });
+    return obj;
+  });
+
+  const renderVirtualList = (rawData: any, { ref, onScroll }: any) => {
+    ref.current = connectObject;
     return (
-      <Table
-        {...props}
-        rowSelection={undefined}
-        columns={mergedColumns}
-        components={{
-          body: renderVirtualList,
+      <VariableSizeGrid
+        ref={gridRef}
+        className="virtual-grid"
+        height={+y}
+        rowCount={rawData.length}
+        columnCount={mergedColumns.length}
+        rowHeight={() => rowHeights[componentSize]}
+        width={tableWidth}
+        onScroll={({ scrollLeft }) => onScroll({ scrollLeft })}
+        columnWidth={(index) => +mergedColumns[index].width!}
+      >
+        {({ columnIndex, rowIndex, style }) => {
+          const { dataIndex, render } = mergedColumns[columnIndex] as any;
+          const data = rawData[rowIndex];
+          const column = data['' + dataIndex];
+
+          return (
+            <div style={{ padding: columnPadding[componentSize], ...style }} className={styles.virtualColumn}>
+              {render?.(column ?? data, data, rowIndex) || column}
+            </div>
+          );
         }}
-      />
+      </VariableSizeGrid>
     );
-  }
-);
+  };
+
+  return (
+    <Table
+      {...props}
+      rowSelection={undefined}
+      columns={mergedColumns}
+      components={{
+        body: renderVirtualList,
+      }}
+    />
+  );
+});
 
 const rowHeights: any = { large: 54, middle: 46, small: 38 };
 
